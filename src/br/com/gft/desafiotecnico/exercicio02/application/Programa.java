ff68494f4fa68ac828a2697e27858a124006771e
Gustavo <reis.gustavoqueiroz@gmail.com>
2022-07-19 18:13:37 -0300

    exercicios resolvidos

--- src/br/com/gft/desafiotecnico/exercicio02/application/Programa.java
+++ src/br/com/gft/desafiotecnico/exercicio02/application/Programa.java
@@ -1,6 +1,22 @@
 package br.com.gft.desafiotecnico.exercicio02.application;
 
+import java.util.Scanner;
+
 public class Programa {
     public static void main(String[] args) {
+
+        Scanner digitar = new Scanner(System.in);
+        int numero1;
+        numero1 = digitar.nextInt();
+        int numero2;
+        numero2 = digitar.nextInt();
+        int numero3;
+        numero3 = digitar.nextInt();
+        int numero4;
+        numero4 = digitar.nextInt();
+        int numero5;
+        numero5 = digitar.nextInt();
+        int numero6;
+        numero6 = digitar.nextInt();
     }
 }
--- src/br/com/gft/desafiotecnico/exercicio03/application/Programa.java
+++ src/br/com/gft/desafiotecnico/exercicio03/application/Programa.java
@@ -3,5 +3,7 @@ package br.com.gft.desafiotecnico.exercicio03.application;
 public class Programa {
     public static void main(String[] args) {
 
+        //Extenso numero = new Extenso();
+        //numero.converterPorExtenso(155);
     }
 }
